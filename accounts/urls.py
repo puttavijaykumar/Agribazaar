28c6e1d5881997ac5ec15a8e6cd14f12be988e22
admin <vijaykumarputta08@gmail.com>
2025-03-13 23:55:51 +0530

    Fix homepage issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from .views import register_view, login_view, logout_view
 from .views import register_view, verify_email
-from .views import home
+from accounts.views import home
 
 urlpatterns = [
-    path("", home, name="home"),  # Homepage
+    path('', home, name='home'),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
