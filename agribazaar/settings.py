31f2d630f104ebea1d577feed4d95e99c761cadd
admin <vijaykumarputta08@gmail.com>
2025-03-15 13:50:03 +0530

    testing

--- agribazaar/settings.py
+++ agribazaar/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url 
+
+# import dj_database_url 
 # Security settings
 # Database configuration
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
