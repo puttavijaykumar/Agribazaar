b1a68d456ecd549f3bbbe6743770cfa4a8757822
admin <vijaykumarputta08@gmail.com>
2025-03-22 00:09:43 +0530

    test

--- agribazaar/settings.py
+++ agribazaar/settings.py
@@ -9,15 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
 import os
 import dj_database_url
 
-
-
  
-
+LOGIN_URL = '/login/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
