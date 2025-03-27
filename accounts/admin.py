d0a99a4ff28a58166e99fc63cf5bc1453bf4cf5b
admin <vijaykumarputta08@gmail.com>
2025-03-27 15:58:55 +0530

    dashboards

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CustomUser,Role
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+       ("User Roles", {"fields": ("roles",)}),
+    )
+    filter_horizontal = ("roles",)
+    
+admin.site.register(Role) 
+admin.site.register(CustomUser) 
+
+
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -4,15 +4,32 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15,unique=True)
-    is_farmer = models.BooleanField(default=False)
-    is_buyer = models.BooleanField(default=False)
+class Role(models.Model):
+    name = models.CharField(max_length=15, unique=True)
     
     def __str__(self):
-        return f" Username: {self.username}, Email: {self.email}, Phone: {self.phone_number}, role:{self.is_farmer or self.is_buyer}"
+        return self.name
+    
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15,unique=True)  
+    roles = models.ManyToManyField(Role, related_name="users")
+    
+    def __str__(self):
+        return f" Username: {self.username}, Email: {self.email}, Phone: {self.phone_number}, roles: {', '.join(role.name for role in self.roles.all())}"
     
 class product_farmer(models.Model):
+    productName = models.CharField(max_length=50)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    images = models.ImageField(upload_to='product_images/')
+    product_vedio = models.FileField(upload_to='product_vedios/',null=True,blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    product_farmer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  
+    def __str__(self):
+        return f"Product Name: {self.productName}, Price: {self.price}, Quantity: {self.quantity}, Description: {self.description}, Farmer: {self.product_farmer}"
+    
+class product_show(models.Model):
     productName = models.CharField(max_length=50)
     price = models.IntegerField()
     quantity = models.IntegerField()
