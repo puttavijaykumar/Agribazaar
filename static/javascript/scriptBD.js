c86b1b441a77bcd419f0759c39fed7d70e9c24a3
admin <vijaykumarputta08@gmail.com>
2025-04-02 17:45:01 +0530

    testing

--- static/javascript/scriptBD.js
+++
@@ -1,7 +0,0 @@
- // Auto-slide banner
- const banners = ["banner1.png","banner2.png","banner3.png"];
- let bannerIndex = 0;
- setInterval(() => {
-     bannerIndex = (bannerIndex + 1) % banners.length;
-     document.getElementById("banner-img").src = banners[bannerIndex];
- }, 3000);
