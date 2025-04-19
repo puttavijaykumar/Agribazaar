c62bf346c61f11ce837278bec727bd50826458d3
admin <vijaykumarputta08@gmail.com>
2025-04-20 01:45:27 +0530

    Testing

--- accounts/models.py
+++ accounts/models.py
@@ -89,7 +89,6 @@ class MarketplaceProduct(models.Model):
         ('equipment', 'Equipment'),
         ('organic', 'Organic'),
     ]
-
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     price = models.DecimalField(max_digits=10, decimal_places=2)
