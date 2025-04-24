93c7f170228f17b15eeb3df480d4d84c97105450
admin <vijaykumarputta08@gmail.com>
2025-04-24 14:15:31 +0530

    testing

--- static/javascript/scriptPLF.js
+++ static/javascript/scriptPLF.js
@@ -7,3 +7,9 @@ document.getElementById("images").addEventListener("change", function (event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 });
+setTimeout(() => {
+    const flashMessages = document.querySelector('.flash-messages');
+    if (flashMessages) {
+        flashMessages.style.display = 'none';
+    }
+}, 3000); // 3 seconds
