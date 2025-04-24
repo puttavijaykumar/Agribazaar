d0e86b3772e85128d683d94818f7e9ae795e0efb
admin <vijaykumarputta08@gmail.com>
2025-04-24 12:40:35 +0530

    testing

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CustomUser,Role
 from django.contrib.auth.admin import UserAdmin
 from .models import Offer, MarketPrice,MarketplaceProduct,product_farmer
+from .models import Negotiation,NegotiationMessage,NegotiationSetting
 
 
 class CustomUserAdmin(UserAdmin):
@@ -17,6 +18,9 @@ admin.site.register(Offer)
 admin.site.register(MarketPrice)
 admin.site.register(MarketplaceProduct)
 admin.site.register(product_farmer)
+admin.site.register(Negotiation)
+admin.site.register(NegotiationMessage)
+admin.site.register(NegotiationSetting)
 
 
 
