8754e82ed78c1d1b6ad46b25a7ad7c82d321c487
admin <vijaykumarputta08@gmail.com>
2025-08-25 23:33:21 +0530

    feat: Completed user activity dashboard with personal and admin views

--- agribazaar/settings.py
+++ agribazaar/settings.py
@@ -90,6 +90,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.ActivityLogMiddleware',
+
 ]
 
 ROOT_URLCONF = 'agribazaar.urls'
