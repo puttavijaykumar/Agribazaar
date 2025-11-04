7ad301b50142bc0ae04ee4df6bb0e03c3a2d7c97
admin <vijaykumarputta08@gmail.com>
2025-11-04 22:51:13 +0530

    testing

--- agribazaar/build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install Python dependencies for the backend
-pip install -r agribazaar/requirements.txt
-
-# # Collect Django static files
-# python agribazaar/manage.py collectstatic --noinput --clear
