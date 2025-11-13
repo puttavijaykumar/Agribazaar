a7e5ba928af5067bd00ef70350943f77371e7b3d
admin <vijaykumarputta08@gmail.com>
2025-11-13 23:57:00 +0530

    testing

--- agribazaar-frontend/src/services/authHeader.js
+++ agribazaar-frontend/src/services/authHeader.js
@@ -1,5 +1,6 @@
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
+  console.log('authHeader user:', user);
   if (user && user.access) {
     return { Authorization: 'Bearer ' + user.access };
   } else {
