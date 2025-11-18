ceba8ed131d269e6ff070cb30049bf53faca145c
admin <vijaykumarputta08@gmail.com>
2025-11-18 10:17:06 +0530

    testing

--- agribazaar-frontend/src/pages/UploadProducts.jsx
+++ agribazaar-frontend/src/pages/UploadProducts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AuthService from "../services/AuthService";
 import EnhancedFooter from "../components/EnhancedFooter";
 import FarmerNavbar from '../components/FarmerNavbar'; // Adjust path as required
